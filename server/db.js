83a57f58ba2fd107e85c01b7812165a4725df2b6
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-09-14 00:54:50 +0100

    Update db.js
    Better use of prepared statements. Next step - put it into a transaction ;)

--- server/db.js
+++ server/db.js
@@ -5,18 +5,17 @@ const PS = require('pg-promise').PreparedStatement;
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/users';
 const db = pgp(connectionString);
 
+const findUser = new PS('find-user', 'SELECT * FROM Users WHERE email = $1');
+const addUser = new PS('add-user', 'INSERT INTO Users(first_name, last_name, password, email, zip) VALUES($1, $2, $3, $4, $5)');
+
 const checkUser = (req, res) => {
   console.log('req body', req.body);
   const userEmail = req.body.email;
   console.log('email', userEmail);
-  const findUser = new PS('find-user', 'SELECT * FROM Users WHERE email = $1', [userEmail]);
 
-  return db.none(findUser)
+  return db.none(findUser, userEmail)
     .then(() => {
-      const addUser = new PS('add-user', 'INSERT INTO Users(first_name, last_name, password, email, zip) VALUES($1, $2, $3, $4, $5)');
-      addUser.values = [req.body.first_name, req.body.last_name, req.body.password, req.body.email, req.body.zip];
-
-      return db.none(addUser)
+      return db.none(addUser, [req.body.first_name, req.body.last_name, req.body.password, req.body.email, req.body.zip])
         .then(() => {
           res.status(201).send('User added');
         })
